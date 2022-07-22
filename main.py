c4ab14b7c049b938890c8d56f58bb921ea181e09
Swiss professional sh!ta$$ <76264518+kevadesu@users.noreply.github.com>
2022-07-22 18:45:07 +0200

    iC-FIX_BUGS && iC_NEW_FEATURE: Finally.

--- main.py
+++ main.py
@@ -14,43 +14,54 @@ class MainWindow(QMainWindow):
         self.showMaximized()
 
         # navbar
-        # space taker
-        navbar = QToolBar()
-        self.addToolBar(navbar)
+        self.navbar = QToolBar()
+        self.navbar.setMovable(False)
+        # make it 50px high
+        self.navbar.setIconSize(QSize(50, 50))
+        self.addToolBar(self.navbar)
 
         back_btn = QAction('[<]', self)
         back_btn.triggered.connect(self.browser.back)
-        navbar.addAction(back_btn)
+        self.navbar.addAction(back_btn)
 
         forward_btn = QAction('[>]', self)
         forward_btn.triggered.connect(self.browser.forward)
-        navbar.addAction(forward_btn)
+        self.navbar.addAction(forward_btn)
 
         reload_btn = QAction('[Ø]', self)
         reload_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(reload_btn)
+        self.navbar.addAction(reload_btn)
 
         home_btn = QAction('[^]', self)
         home_btn.triggered.connect(self.navigate_home)
-        navbar.addAction(home_btn)
+        self.navbar.addAction(home_btn)
 
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
-        navbar.addWidget(self.url_bar)
+        self.navbar.addWidget(self.url_bar)
 
         self.browser.urlChanged.connect(self.update_url)
         
+        # tab switcher
+        self.tab_switcher = QComboBox()
+        self.tab_switcher.currentIndexChanged.connect(self.tabSwitcherProcess)
+        self.navbar.addWidget(self.tab_switcher)
+        self.tab_switcher.setFixedWidth(250)
 
-		# new tab button
-		new_tab_btn = QAction("[+]", self)
-		new_tab_btn.triggered.connect(self.newTabProcess)
-		self.navbar.addAction(new_tab_btn)
+        # add new tab to the tab switcher
+        self.tab_switcher.addItem("New tab")
+        self.tab_switcher.setCurrentIndex(0)
 
-		# close tab button
-		close_tab_btn = QAction("[×]", self)
-		close_tab_btn.triggered.connect(self.closeTabProcess)
-		self.navbar.addAction(close_tab_btn)
-        
+        # new tab button
+        new_tab_btn = QAction("[+]", self)
+        new_tab_btn.triggered.connect(self.newTabProcess)
+        self.navbar.addAction(new_tab_btn)
+
+        # close tab button
+        close_tab_btn = QAction("[×]", self)
+        close_tab_btn.triggered.connect(self.closeTabProcess)
+        self.navbar.addAction(close_tab_btn)
+
     def navigate_home(self):
         self.browser.setUrl(QUrl('https://lintine.github.io/newtab/'))
 
@@ -60,36 +71,36 @@ class MainWindow(QMainWindow):
 
     def update_url(self, q):
         self.url_bar.setText(q.toString())
-        
+
     def tabSwitcherProcess(self):
-		self.browser_index = self.tab_switcher.currentIndex()
-		# show the correct browser
-		self.takeCentralWidget()
-		self.setCentralWidget(self.browsers[self.browser_index])
-		# set the window title to "Blazenet • " + page title + " • " + url
-		self.setWindowTitle("Minisurfer Window • " + self.browser().page().title() + " • " + self.browser().url().toString())
-	
-	def newTabProcess(self):
-		self.browsers.append(QWebEngineView())
-		self.tab_switcher.addItem("New Tab")
-		self.tab_switcher.setCurrentIndex(len(self.browsers) - 1)
-		self.tabSwitcherProcess()
-		# when the page changes, call onPageChangedProcess()
-		self.browser().urlChanged.connect(self.onPageChangedProcess)
-		# when the browser loads a page, call onPageLoadProcess()
-		self.browser().loadFinished.connect(self.onPageLoadProcess)
-		# homepage
-		try: self.homeProcess()
-		except: pass
-	
-	def closeTabProcess(self):
-		if len(self.browsers) > 1:
-			self.browsers.pop(self.browser_index)
-			self.tab_switcher.removeItem(self.browser_index)
-			if self.browser_index >= len(self.browsers):
-				self.browser_index = len(self.browsers) - 1
-			self.tabSwitcherProcess()
-	
+        self.browser_index = self.tab_switcher.currentIndex()
+        # show the correct browser
+        self.takeCentralWidget()
+        self.setCentralWidget(self.browsers[self.browser_index])
+        # set the window title to "Minisurfer Window • " + page title + " • " + url
+        self.setWindowTitle("Minisurfer Window • " + self.browser().page().title() + " • " + self.browser().url().toString())
+
+    def newTabProcess(self):
+        self.browsers.append(QWebEngineView())
+        self.tab_switcher.addItem("New Tab")
+        self.tab_switcher.setCurrentIndex(len(self.browsers) - 1)
+        self.tabSwitcherProcess()
+        # when the page changes, call onPageChangedProcess()
+        self.browser().urlChanged.connect(self.onPageChangedProcess)
+        # when the browser loads a page, call onPageLoadProcess()
+        self.browser().loadFinished.connect(self.onPageLoadProcess)
+        # homepage
+        try: self.homeProcess()
+        except: pass
+
+    def closeTabProcess(self):
+                if len(self.browsers) > 1:
+                        self.browsers.pop(self.browser_index)
+                        self.tab_switcher.removeItem(self.browser_index)
+                        if self.browser_index >= len(self.browsers):
+                                self.browser_index = len(self.browsers) - 1
+                        self.tabSwitcherProcess()
+
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Minisurfer')
